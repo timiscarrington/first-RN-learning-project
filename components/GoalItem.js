810d5740b6a3286d4200c54e83e034ec9d9775d8
timiscarrington <timiscarrington@gmail.com>
2023-07-19 11:46:19 -0700

    styling added to items when pressed for deletion to indicate selection

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,11 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem (props){
 return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
     <View style={styles.goalItem}>
+    <Pressable  android_ripple={{ color: '#928181'}} onPress={props.onDeleteItem.bind(this, props.id)} style={({pressed}) => pressed && styles.pressedItem}>
     <Text style={styles.goalItemText}>{props.text}</Text>
-    </View>
     </Pressable>
+    </View>
 )
 }
 
@@ -17,9 +17,12 @@ const styles = StyleSheet.create({
         margin: 8,
         borderRadius: 6,
         backgroundColor: "#5e0acc",
-        padding: 8,
+      },
+      pressedItem: {
+        opacity: 0.5
       },
       goalItemText: {
-        color: 'white'
+        color: 'white',
+        padding: 8
       }
 });
